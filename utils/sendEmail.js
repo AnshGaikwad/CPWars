98fa118a3c7c4027a0164634dd776579aedc89d6
AnshGaikwad <anshyg2002@gmail.com>
2021-05-16 12:07:27 +0530

    fixed forgot password using sendgrid web api

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,28 +1,24 @@
-const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail')
 
 const sendEmail = (options) => {
-  const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
 
-  const mailOptions = {
-    from: process.env.EMAIL_FROM,
-    to: options.to,
-    subject: options.subject,
-    html: options.text,
-  };
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-  transporter.sendMail(mailOptions, function (err, info) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(info);
-    }
-  });
+const mailOptions = {
+  from: process.env.EMAIL_FROM,
+  to: options.to,
+  subject: options.subject,
+  html: options.text,
+};
+
+sgMail
+  .send(mailOptions)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
 };
 
 module.exports = sendEmail;
