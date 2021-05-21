1cb1cd29fab2c7e8a771e5c65e6bf1ef64ba07ce
AnshGaikwad <anshyg2002@gmail.com>
2021-05-21 15:24:33 +0530

    sidebar implemented

--- client/src/components/screens/HomeScreen.js
+++ client/src/components/screens/HomeScreen.js
@@ -1,6 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./HomeScreen.css";
+import Sidebar from "../sidebar/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Overview, { Battle, Practice, Tournaments } from "../../pages/Overview";
+import { CodeChef, GeeksForGeeks, HackerRank, Learn } from "../../pages/Learn";
+import Watch from "../../pages/Watch";
+import Profile from "../../pages/Profile";
+import { Coders, Community, Forum, Teams } from "../../pages/Community";
+import Support from "../../pages/Support";
 
 const HomeScreen = () => {
   const [error, setError] = useState("");
@@ -26,10 +34,31 @@ const HomeScreen = () => {
 
     fetchHomeDate();
   }, []);
+
+
   return error ? (
     <span className="error-message">{error}</span>
   ) : (
-    <div>{homeData}</div>
+    <Router>
+      <Sidebar />
+      <Switch>
+        <Route path='/overview' exact component={Overview} />
+        <Route path='/overview/battle' exact component={Battle} />
+        <Route path='/overview/practice' exact component={Practice} />
+        <Route path='/overview/tournaments' exact component={Tournaments} />
+        <Route path='/learn' exact component={Learn} />
+        <Route path='/learn/geeksforgeeks' exact component={GeeksForGeeks} />
+        <Route path='/learn/hackerrank' exact component={HackerRank} />
+        <Route path='/learn/codechef' exact component={CodeChef} />
+        <Route path='/watch' exact component={Watch} />
+        <Route path='/profile' exact component={Profile} />
+        <Route path='/community' exact component={Community} />
+        <Route path='/community/coders' exact component={Coders} />
+        <Route path='/community/teams' exact component={Teams} />
+        <Route path='/community/forum' exact component={Forum} />
+        <Route path='/support' exact component={Support} />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ client/src/pages/Overview.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Overview = () => {
+  return (
+    <div className='main'>
+      <h1>Overview</h1>
+    </div>
+  );
+};
+
+export const Battle = () => {
+    return (
+      <div className='main'>
+        <h1>Battle</h1>
+      </div>
+    );
+  };
+  
+  export const Practice = () => {
+    return (
+      <div className='main'>
+        <h1>Practice</h1>
+      </div>
+    );
+  };
+  
+  export const Tournaments = () => {
+    return (
+      <div className='main'>
+        <h1>Tournaments</h1>
+      </div>
+    );
+  };
+
+export default Overview;
