3a77315a7ebc5a93fe97c300b1d6927bd36fd221
AnshGaikwad <anshyg2002@gmail.com>
2021-05-24 10:39:25 +0530

    commented server compiler code

--- server/server.js
+++ server/server.js
@@ -7,10 +7,10 @@ const errorHandler = require("./middleware/error");
 
 connectDB();
 
-const { generateFile } = require("./compiler/generateFile");
+// const { generateFile } = require("./compiler/generateFile");
 
-const { addJobToQueue } = require("./compiler/jobQueue");
-const Job = require("./models/Job");
+// const { addJobToQueue } = require("./compiler/jobQueue");
+// const Job = require("./models/Job");
 
 
 app.use(cors());
@@ -26,40 +26,40 @@ app.get("/", (req, res, next) => {
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/home", require("./routes/home"));
 
-app.post("/run", async (req, res) => {
-  const { language = "cpp", code } = req.body;
+// app.post("/run", async (req, res) => {
+//   const { language = "cpp", code } = req.body;
 
-  console.log(language, "Length:", code.length);
+//   console.log(language, "Length:", code.length);
 
-  if (code === undefined) {
-    return res.status(400).json({ success: false, error: "Empty code body!" });
-  }
-  // need to generate a c++ file with content from the request
-  const filepath = await generateFile(language, code);
-  // write into DB
-  const job = await new Job({ language, filepath }).save();
-  const jobId = job["_id"];
-  addJobToQueue(jobId);
-  res.status(201).json({ jobId });
-});
+//   if (code === undefined) {
+//     return res.status(400).json({ success: false, error: "Empty code body!" });
+//   }
+//   // need to generate a c++ file with content from the request
+//   const filepath = await generateFile(language, code);
+//   // write into DB
+//   const job = await new Job({ language, filepath }).save();
+//   const jobId = job["_id"];
+//   addJobToQueue(jobId);
+//   res.status(201).json({ jobId });
+// });
 
-app.get("/status", async (req, res) => {
-  const jobId = req.query.id;
+// app.get("/status", async (req, res) => {
+//   const jobId = req.query.id;
 
-  if (jobId === undefined) {
-    return res
-      .status(400)
-      .json({ success: false, error: "missing id query param" });
-  }
+//   if (jobId === undefined) {
+//     return res
+//       .status(400)
+//       .json({ success: false, error: "missing id query param" });
+//   }
 
-  const job = await Job.findById(jobId);
+//   const job = await Job.findById(jobId);
 
-  if (job === undefined) {
-    return res.status(400).json({ success: false, error: "couldn't find job" });
-  }
+//   if (job === undefined) {
+//     return res.status(400).json({ success: false, error: "couldn't find job" });
+//   }
 
-  return res.status(200).json({ success: true, job });
-});
+//   return res.status(200).json({ success: true, job });
+// });
 
 
 // Error Handler Middleware
